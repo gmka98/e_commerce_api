56e50d8ef07bb5f2165ddf70462fdafca879be94
gmk <gmka98@gmail.com>
2024-10-02 12:58:03 +0200

    registration done

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
+from tortoise.signals import post_save
+from typing import List, Optional, Type
 from models import *
-
+from authentication import (get_hashed_password)
+from tortoise import BaseDBAsyncClient
+from tortoise.contrib.fastapi import register_tortoise
 app = FastAPI()
 
+@post_save(User)
+async def create_business(
+    sender: "Type[User]",
+    instance: User,
+    created: bool,
+    using_db: "Optional[BaseDBAsyncClient]",
+    update_fields: List[str]
+) -> None:
+    
+    if created:
+        business_obj = await Business.create(
+            business_name=instance.username, owner=instance
+        )
+        # send the email
+        await business_pydantic.from_tortoise_orm(business_obj)
+
+
+@app.post("/registration")
+async def user_registration(user: user_pydanticIn):
+    user_info = user.dict(exclude_unset=True) 
+    user_info["password"] = get_hashed_password(user_info["password"])
+    user_object = await User.create(**user_info)
+    new_user = await user_pydantic.from_tortoise_orm(user_object)
+    return{
+        "status" : "ok",
+        "data" : f"Hello {new_user.username} thanks for choosing our services. Please check your email inbox to and click on the link to confirm your registration."
+    }
+
 @app.get("/")
-def first():
+def index():
     return{"Hello" : "World"}
 
 register_tortoise(
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from tortoise import Model, fields
+from tortoise import fields
+from tortoise.models import Model
 from pydantic import BaseModel
 from datetime import datetime
 from tortoise.contrib.pydantic import pydantic_model_creator
@@ -10,12 +11,13 @@ class User(Model):
     email = fields. CharField(max_length=200, null = False, unique = True)
     password = fields.CharField(max_length=100, null = False)
     is_verified = fields.BooleanField(default = False)
-    join_data = fields.DatetimeField(default = datetime.utcnow)
+    join_date = fields.DatetimeField(default = datetime.utcnow())
+
 
 
 class Business(Model):
     id = fields.IntField(pk = True, index = True)
-    Business_name = fields.CharField
+    business_name = fields.CharField(max_length=20, null=False, unique = True)
     city = fields.CharField(max_length=100, null = False, default = "Unspecified")
     region = fields.CharField(max_length=100, null = False, default = "Unspecified")
     Business_description = fields.TextField(null = True )
@@ -29,14 +31,14 @@ class Product(Model):
     original_price = fields.DecimalField(max_digits=12, decimal_places=2)
     new_price = fields.DecimalField(max_digits=12, decimal_places=2)
     percentage_discount = fields.IntField()
-    offer_expiration_data = fields.DateField(default = datetime.utcnow)
+    offer_expiration_data = fields.DateField(default = datetime.utcnow())
     product_image = fields.CharField(max_length=200, null = False, default="productDefault.jpg")
     business = fields.ForeignKeyField("models.Business", related_name="products")
 
 
 
 user_pydantic = pydantic_model_creator(User, name = "User", exclude=("is_verified", ))
-user_pydanticIn = pydanticIn = pydantic_model_creator(User, name = "UserIn", exclude_readonly=True)
+user_pydanticIn = pydanticIn = pydantic_model_creator(User, name = "UserIn", exclude_readonly=True, exclude=("is_verified", "join_date"))
 user_pydanticOut = pydantic_model_creator(User, name = "UserOut", exclude=("password", ))
 
 
